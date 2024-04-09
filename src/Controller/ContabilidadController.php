e29a4a1e62d174601e138c868248506cc96ee55f
Alejandro Paton <alejandropatonaraujo2@gmail.com>
2024-04-09 11:29:47 +0000

    Adding templates

--- src/Controller/ContabilidadController.php
+++ src/Controller/ContabilidadController.php
@@ -7,16 +7,30 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContabilidadController extends AbstractController{
-    
-    #[Route('/card', name:'contabilidad_card')]
+
+    #[Route('/', name:'contabilidad_card')]
     public function contabilidadCard() : Response
     {
         return $this->render('card.html.twig');
     }
 
-    #[Route('/contabilidad', name:'contabilidad_main')]
-    public function contabilidadMain() : Response
-    {
+    #[Route('/contabilidad', name:'contabilidad')]
+    public function index():Response{
         return $this->render('main.html.twig');
+    }
+
+    #[Route('/contabilidad/section1', name:'contabilidad_section1')]
+    public function section1() : Response{
+        return $this->render('section1.html.twig');
+    }
+    
+    #[Route('/contabilidad/section2', name:'contabilidad_section2')]
+    public function section2() : Response{
+        return $this->render('section2.html.twig');
+    }
+
+    #[Route('/contabilidad/section3', name:'contabilidad_section3')]
+    public function section3() : Response{
+        return $this->render('section3.html.twig');
     }
 }
